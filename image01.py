cd2b605968134c132bf9513f36b23fb8824984cc
mimige-man <118783296+mimige-man@users.noreply.github.com>
2023-07-03 13:49:41 +0900

    Add files via upload

---
+++ image01.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+
+hugging_token = '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'
+
+ldm = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4",
+                                              revision="fp16",
+                                              torch_dtype=torch.float16,
+                                              use_auth_token=hugging_token
+                                              ).to("cuda")
+
+prompt = '<生成したい画像を表現した、文字列>'
+
+# 1000枚画像を作りたい場合
+num_images = 1000
+for j in range(num_images):
+    with autocast("cuda"):
+        image = ldm(prompt).images[0] # 500×500px画像が生成
+        # 画像サイズを変更したい場合
+        # image = ldm(prompt, height=400, width=400).images[0]
+
+    # save images (本コードでは、直下に画像が生成されていきます。)
+    image.save(f"./image_{i}.png")
